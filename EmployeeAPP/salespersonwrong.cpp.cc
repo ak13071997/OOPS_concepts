ee33782b24b5becc6d2f886c23c3e1a24ae917d1
ak13071997 <146116238+ak13071997@users.noreply.github.com>
2023-09-30 21:01:40 +0530

    OOPS concepts programs

---
+++ EmployeeAPP/salespersonwrong.cpp.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"salesperson.h"
+using namespace std;
+
+salesperson::salesperson()    //default constructor will automatically call all the attribues
+{
+   salary=12500;
+   
+}
